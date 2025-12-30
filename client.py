16c3d916efa53c56fc851b4110cf245200301fef
Ruben Felgenhauer <Ruben.Felgenhauer@posteo.de>
2025-12-30 16:32:19 +0100

    Add some validation logic

--- client.py
+++ client.py
@@ -20,15 +20,20 @@ def main() -> None:
     try:
         print(api_client.read_item())
         raise RuntimeError("Too few parameters! This should throw, but it didn't!")
-    except HTTPError:
+    except (HTTPError, ValueError):
         pass
 
     try:
         print(api_client.read_item(item_id=42, q="Foo", foo="bar"))
         raise RuntimeError("Too many parameters! This should throw, but it didn't!")
-    except HTTPError:
+    except (HTTPError, ValueError):
         pass
 
+    try:
+        print(api_client.read_item(item_id=42.5, q="Foo"))
+        raise RuntimeError("Wrong type! This should throw, but it didn't!")
+    except (HTTPError, ValueError):
+        pass
 
 if __name__ == "__main__":
     main()
--- typed_rest.py
+++ typed_rest.py
@@ -3,7 +3,7 @@
 from dataclasses import dataclass
 from enum import StrEnum
 from typing import assert_never
-
+from pydantic import TypeAdapter, ValidationError
 
 @dataclass
 class Route:
@@ -21,6 +21,7 @@ class ApiDefinition:
         def route_decorator(func):
             name = func.__name__
             annotations = func.__annotations__
+            assert ("return" in annotations), (f"Unable to add route without return type!")
             assert name not in self.routes, f'Duplicate route "{name}"'
             self.routes[name] = Route(method, path, name, annotations)
             return func
@@ -46,6 +47,9 @@ class ApiImplementation:
             f'Handler "{name}" does not match any defined routes.'
         )
         route_def = self.api_def.routes[name]
+        # TODO: Make the following assertion more lax in the future.
+        # Ignore return type, only the parameter names must match.
+        # But: We have to overwrite the annotations with the route annotations before passing the function to FastAPI!
         assert annotations == route_def.annotations, (
             f'Annotations for handler "{name}" don\'t match the corresponding route. Expected: {route_def.annotations}. Received: {annotations}.'
         )
@@ -77,11 +81,21 @@ class ApiClientEngine(StrEnum):
 class ApiClient:
     def make_request_with_requests(self, route: Route, *args, **kwargs):
         import requests
-
         path = route.path
-        param_names = [name for name in route.annotations.keys() if name != "return"]
-        for name, value in zip(param_names, args):
+        assert "return" in route.annotations
+        expected_parameters = {name: ptype for (name, ptype) in route.annotations.items() if name != "return"}
+        # param_names = [name for name in route.annotations.keys() if name != "return"]
+        # TODO: Check parameters (name, order and type -> raise ValueError)
+        if len(args) + len(kwargs) > len(expected_parameters):
+            raise ValueError(f"Received too many parameters. Expected {len(expected_parameters)}, but received {len(args) + len(kwargs)}")
+        for name, value in zip(expected_parameters.keys(), args):
             kwargs.setdefault(name, value)
+        for (name, ptype), value in zip(expected_parameters.items(), kwargs.values()):
+            type_adapter = TypeAdapter(ptype)
+            try:
+                _ = type_adapter.validate_python(value)
+            except ValidationError as e:
+                raise ValueError(f"Detected illegal type for parameter \"{name}\". Expected {ptype}, but received {value.__class__}") from e
         for name, value in list(kwargs.items()):
             placeholder = "{" + name + "}"
             if placeholder in path:
@@ -96,7 +110,9 @@ class ApiClient:
         )
         response.raise_for_status()
         json = response.json()
-        return json
+        return_type = route.annotations["return"]
+        type_adapter = TypeAdapter(return_type)
+        return type_adapter.validate_python(json)
 
     def __init__(self, api_def: ApiDefinition, engine: str, base_url: str):
         assert engine in ApiClientEngine, (
