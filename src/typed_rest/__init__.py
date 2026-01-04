fc1513c6a8912ae8e535ea67cc3898c72c898086
Ruben Felgenhauer <Ruben.Felgenhauer@posteo.de>
2026-01-04 12:38:58 +0100

    Add RequestParam class to handle Path(), Query(), etc...

--- src/typed_rest/__init__.py
+++ src/typed_rest/__init__.py
@@ -1,11 +1,56 @@
+import inspect
+import re
+from collections.abc import Callable
 from dataclasses import dataclass
 from enum import StrEnum
-from typing import assert_never
+from typing import Any, Optional, Union, assert_never
+
 import pydantic
-from pydantic import TypeAdapter
-import inspect
-from collections.abc import Callable
-import re
+from pydantic import AliasChoices, AliasPath, AnyUrl, TypeAdapter
+from pydantic_core import PydanticUndefined
+from typing_extensions import TypedDict, deprecated
+
+
+class ParamExample(TypedDict, total=False):
+    summary: Optional[str]
+    description: Optional[str]
+    value: Optional[Any]
+    externalValue: Optional[AnyUrl]
+    __pydantic_config__ = {"extra": "allow"}  # type: ignore[misc]
+
+
+class RequestParam:
+    def __init__(self, *args, **kwargs):
+        def f(
+            *,
+            alias: Optional[str] = None,
+            alias_priority: Union[int, None] = PydanticUndefined,
+            validation_alias: Union[str, AliasPath, AliasChoices, None] = None,
+            serialization_alias: Union[str, None] = None,
+            title: Optional[str] = None,
+            description: Optional[str] = None,
+            discriminator: Union[str, None] = None,
+            examples: Optional[list[Any]] = None,
+            openapi_examples: Optional[dict[str, ParamExample]] = None,
+            deprecated: Union[deprecated, str, bool, None] = None,
+            include_in_schema: bool = True,
+            json_schema_extra: Union[dict[str, Any], None] = None,
+        ): ...
+
+        signature = inspect.signature(f)
+        self.bound_args = signature.bind(*args, **kwargs)
+
+
+class Path(RequestParam): ...
+
+
+class Query(RequestParam): ...
+
+
+class Body(RequestParam): ...
+
+
+class Header(RequestParam): ...
 
 
 @dataclass
@@ -302,8 +347,9 @@ class ApiClient:
         self._add_accessor(route, transport)
 
     def _add_accessor_with_testclient(self, route: Route):
-        import httpx
         import json
+
+        import httpx
 
         def transport(method: str, path: str, params: dict | None):
             url = path
