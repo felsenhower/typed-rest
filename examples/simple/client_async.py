e384548f42ca822c9932930fcef2b812becd08e3
Ruben Felgenhauer <Ruben.Felgenhauer@posteo.de>
2026-01-05 20:11:59 +0100

    Update example "simple"

---
+++ examples/simple/client_async.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from typed_rest import ApiClient
+
+from my_api import api_def
+
+import asyncio
+
+import aiohttp
+
+async def main() -> None:
+    async with aiohttp.ClientSession() as session:
+        api_client = ApiClient(api_def, engine="aiohttp", session=session, base_url="http://127.0.0.1:8000")
+        result = await api_client.read_root()
+        print(result)
+        assert result == {"Hello": "World"}
+        result2 = await api_client.read_item(item_id=42, q="Foo")
+        print(result2)
+        assert result2 == {"item_id": 42, "q": "Foo"}
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- examples/simple/client_sync.py
+++ examples/simple/client_sync.py
@@ -7,8 +7,12 @@ from my_api import api_def
 
 def main() -> None:
     api_client = ApiClient(api_def, engine="requests", base_url="http://127.0.0.1:8000")
-    print(api_client.read_root())
-    print(api_client.read_item(item_id=42, q="Foo"))
+    result = api_client.read_root()
+    print(result)
+    assert result == {"Hello": "World"}
+    result2 = api_client.read_item(item_id=42, q="Foo")
+    print(result2)
+    assert result2 == {"item_id": 42, "q": "Foo"}
 
 
 if __name__ == "__main__":
